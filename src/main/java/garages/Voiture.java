911a72ddcf9fa2fec574ba63e8bf61180ce71747
adrien izunsky <adrien.izunsky@etud.univ-jfc.fr>
2024-11-14 09:53:09 +0100

    TP7fini

--- src/main/java/garages/Voiture.java
+++ src/main/java/garages/Voiture.java
@@ -8,7 +8,6 @@ import lombok.ToString;
 import java.io.PrintStream;
 import java.util.*;
 
-
 /**
  * Représente une voiture qui peut être stationnée dans des garages.
  */
@@ -19,7 +18,9 @@ public class Voiture {
 	@Getter
 	@NonNull
 	private final String immatriculation;
-	@ToString.Exclude // On ne veut pas afficher les stationnements dans toString
+
+	// Liste des stationnements de cette voiture
+	@ToString.Exclude
 	private final List<Stationnement> myStationnements = new LinkedList<>();
 
 	/**
@@ -30,8 +31,12 @@ public class Voiture {
 	 * @throws IllegalStateException Si déjà dans un garage
 	 */
 	public void entreAuGarage(Garage g) throws IllegalStateException {
-		// Et si la voiture est déjà dans un garage ?
+		// Vérifie si la voiture est déjà dans un garage
+		if (estDansUnGarage()) {
+			throw new IllegalStateException("La voiture est déjà dans un garage.");
+		}
 
+		// Crée un nouveau stationnement pour la voiture dans le garage spécifié
 		Stationnement s = new Stationnement(this, g);
 		myStationnements.add(s);
 	}
@@ -43,55 +48,60 @@ public class Voiture {
 	 * @throws IllegalStateException si la voiture n'est pas dans un garage
 	 */
 	public void sortDuGarage() throws IllegalStateException {
-		throw new UnsupportedOperationException("Pas encore implémenté");
-		// TODO: Implémenter cette méthode
-		// Trouver le dernier stationnement de la voiture
-		// Terminer ce stationnement
+		// Vérifie si la voiture est actuellement dans un garage
+		if (!estDansUnGarage()) {
+			throw new IllegalStateException("La voiture n'est pas dans un garage.");
+		}
+
+		// Trouve le dernier stationnement de la voiture et marque sa sortie
+		Stationnement dernierStationnement = myStationnements.get(myStationnements.size() - 1);
+		dernierStationnement.terminer();
 	}
 
 	/**
 	 * Calcule l'ensemble des garages visités par cette voiture
-	 * 
+	 *
 	 * @return l'ensemble des garages visités par cette voiture
 	 */
 	public Set<Garage> garagesVisites() {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		// Crée un ensemble des garages visités en parcourant les stationnements
+		Set<Garage> garages = new HashSet<>();
+		for (Stationnement s : myStationnements) {
+			garages.add(s.getGarageVisite());
+		}
+		return garages;
 	}
 
 	/**
 	 * Détermine si la voiture est actuellement dans un garage
-	 * 
+	 *
 	 * @return vrai si la voiture est dans un garage, faux sinon
 	 */
 	public boolean estDansUnGarage() {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-		// Vrai si il y a des stationnements et le dernier stationnement est en cours
+		// La voiture est dans un garage si elle a des stationnements et si le dernier est en cours
+		return !myStationnements.isEmpty() && myStationnements.get(myStationnements.size() - 1).estEnCours();
 	}
 
 	/**
 	 * Pour chaque garage visité, imprime le nom de ce garage suivi de la liste des
 	 * dates d'entrée / sortie dans ce garage
-	 * <br>
-	 * Exemple :
-	 * 
-	 * <pre>
-	 * Garage(name=Universite Champollion Albi):
-	 * 		Stationnement{ entree=13/11/2024, sortie=13/11/2024 }
-	 * Garage(name=ISIS Castres):
-	 * 		Stationnement{ entree=13/11/2024, sortie=13/11/2024 }
-	 * 		Stationnement{ entree=13/11/2024, en cours }
-	 * </pre>
 	 *
 	 * @param out l'endroit où imprimer (ex: System.out pour imprimer dans la
 	 *            console)
 	 */
 	public void imprimeStationnements(PrintStream out) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-		// Utiliser les méthodes toString() de Garage et Stationnement
+		// Crée une map qui regroupe les stationnements par garage
+		Map<Garage, List<Stationnement>> garagesMap = new HashMap<>();
+		for (Stationnement s : myStationnements) {
+			garagesMap.computeIfAbsent(s.getGarageVisite(), k -> new ArrayList<>()).add(s);
+		}
 
+		// Pour chaque garage, imprime les stationnements associés
+		for (Map.Entry<Garage, List<Stationnement>> entry : garagesMap.entrySet()) {
+			out.println(entry.getKey() + ":");
+			for (Stationnement s : entry.getValue()) {
+				out.println("\t" + s);
+			}
+		}
 	}
-
 }
